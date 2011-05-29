134fee5a7b011c925a4527bea80e61d1eff3cac2
Changsu Lee <lcs6687@naver.com>
2011-05-29 22:18:26 +0900

    GUI 추가(변경)

--- src/com/cyrusian/academic/sra/GUIManager.java
+++ src/com/cyrusian/academic/sra/GUIManager.java
@@ -12,49 +12,33 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 
-public class GUIManager extends JFrame {
-	
-	private class ButtonListener implements ActionListener{
-		
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			JButton b = (JButton)e.getSource();
-			int x = b.getX()/80;
-			int y = b.getY()/80;
-			System.out.println("X : " + x + "  Y : " + y);
-		}
-		
-	}
-	
-	private class JImagePanel extends JPanel {
-		
-		Image image;
-		
-		public JImagePanel(Image image) {
-			this.image = image;
-		}
-		
-		@Override
-		public void paintComponent(Graphics g) {
-			g.drawImage(this.image, 650, 0, this);
-		}
-		
+public class GUIManager implements ActionListener {
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		JButton b = (JButton)e.getSource();
+		int x = b.getX()/80;
+		int y = b.getY()/80;
+		System.out.println("X : " + x + "  Y : " + y);
 	}
 
-	public GUIManager() {
+	public void go() {
 		JFrame frame = new JFrame("SRA - Stalemate Resolution Associate");
-		ImageIcon icon = new ImageIcon("panelImage.png");
-		JButton b[];
+		ImageIcon icon = new ImageIcon("C:/Users/user/Documents/NetBeansProjects/SRA/src/com/cyrusian/academic/sra/panelImage.png");
+		JButton b[] = new JButton[64];
 
 		JPanel panel1 = new JPanel();
-		JImagePanel panel2 = new JImagePanel(icon.getImage());
 		JPanel back = new JPanel();
 		JPanel playground = new JPanel();
+                JImagePanel panel2 = new JImagePanel(icon.getImage());
+                
 		panel1.setBackground(Color.black);
 		panel2.setBackground(Color.lightGray);
+                
 		playground.setBackground(Color.white);
+                
 		panel1.setBounds(0, 0, 650, 650);
 		panel2.setBounds(650,0,250,650);
+                
 		playground.setBounds(5,5,640,640);
 
 		panel1.setVisible(true);
@@ -66,18 +50,20 @@ public class GUIManager extends JFrame {
 		frame.getContentPane().add(panel2);
 
 		playground.setLayout(new GridLayout(8,8));
-		b=new JButton[64];
 		int count=0;
-		for(int i = 0 ; i < 64 ; i++){
-
+		for(int i = 0 ; i < 64 ; i++) {
 			if(count==0) {
-				b[i] = new JButton(new ImageIcon("Chess-Brown.png"));
-				if(i!=15&&i!=31&&i!=47)count=1;
+				b[i] = new JButton(new ImageIcon("C:/Users/user/Documents/NetBeansProjects/SRA/src/com/cyrusian/academic/sra/Chess-Brown.png"));
+				b[i].setBackground(Color.black);
+                                if((i!=15) && (i!=31) && (i!=47))
+					count=1;
 			}
 			else if(count==1) {
-				b[i] = new JButton(new ImageIcon("Chess-LightBrown.png"));
+				b[i] = new JButton(new ImageIcon("C:/Users/user/Documents/NetBeansProjects/SRA/src/com/cyrusian/academic/sra/Chess-LightBrown.png"));
 				b[i].setBackground(Color.white);
-				if(i!=7&&i!=23&&i!=39&&i!=55)count=0;
+                                
+				if((i!=7) && (i!=23) && (i!=39) && (i!=55))
+                                    count=0;
 			}
 			b[i].setRolloverEnabled(false);
 			b[i].setBorderPainted(false);
@@ -89,11 +75,8 @@ public class GUIManager extends JFrame {
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		ButtonListener listener = new ButtonListener();
-		for(int i = 0; i<64; i++){
+		GUIManager listener = new GUIManager();
+		for(int i = 0; i<64; i++)
 			b[i].addActionListener(listener);
-		}
-
   	}
-	
 }
---
+++ src/com/cyrusian/academic/sra/JImagePanel.java
@@ -0,0 +1,16 @@
+package com.cyrusian.academic.sra;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class JImagePanel extends JPanel {
+	Image image;
+
+	public JImagePanel(Image image) {
+		this.image = image;
+	}
+
+	public void paintComponent(Graphics g) {
+		g.drawImage(this.image, 650, 0, this);
+	}
+}
