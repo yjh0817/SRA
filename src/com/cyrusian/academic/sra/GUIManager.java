08a7679da37197d54739f622dd857e3e0f77d4cd
Linus Park <pjk41018@hotmail.com>
2011-05-31 21:12:24 +0900

    Set Menubar

--- src/com/cyrusian/academic/sra/GUIManager.java
+++ src/com/cyrusian/academic/sra/GUIManager.java
@@ -1,14 +1,10 @@
 package com.cyrusian.academic.sra;
 
-import java.awt.Color;
-import java.awt.GridLayout;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.net.URL;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.*;
 
 
 public class GUIManager extends JFrame {
@@ -29,14 +25,50 @@ public class GUIManager extends JFrame {
 		}
 		
 	}
-
+	
+//***************** ActionListener of MenuBar *****************
+	private class exitaction implements ActionListener {
+		public void actionPerformed (ActionEvent e){
+			System.exit(0);
+		}
+	}
+//**************************************************************
 	public GUIManager() {
 		super("SRA");
 		setSize(640, 640); // why 900, 688?
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setResizable(false);
 		setBackground(Color.BLACK);
+		setVisible(true);
 		
+//***************** Menu Bar *********************
+		
+		JMenuBar menubar = new JMenuBar();
+		this.setJMenuBar(menubar);
+
+		JMenu nMenu = new JMenu("Menu");
+		menubar.add(nMenu);
+		JMenuItem nMenu_com = new JMenuItem("New game (compuger)");
+		JMenuItem nMenu_player = new JMenuItem("New game (player)");
+		JMenuItem nMenu_giveup = new JMenuItem("Give up!!");
+		JMenuItem nMenu_exit = new JMenuItem("Exit");
+		nMenu.add(nMenu_com);
+		nMenu.add(nMenu_player);
+		nMenu.add(nMenu_giveup);
+		nMenu.add(nMenu_exit);
+		
+		JMenu difMenu=new JMenu("Difficulty");
+		menubar.add(difMenu);
+		JMenuItem dif_easy=new JMenuItem("Easy");
+		JMenuItem dif_mid=new JMenuItem("Middle");
+		JMenuItem dif_hard=new JMenuItem("Hard");
+		difMenu.add(dif_easy);
+		difMenu.add(dif_mid);
+		difMenu.add(dif_hard);
+		
+		nMenu_exit.addActionListener(new exitaction());
+		
+//****************** Game Board *******************
 		JPanel playGround = new JPanel();
 		playGround.setLayout(new GridLayout(8, 8));
 		
