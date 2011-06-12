a1a754627ab073f5001c5ecac6ebe70d4583bdc0
Jongha Yun <yjh0817@wm.cau.ac.kr>
2011-06-12 19:59:02 +0900

    Complete making getAllocation() of all elemetns

--- src/com/cyrusian/academic/sra/element/Pawn.java
+++ src/com/cyrusian/academic/sra/element/Pawn.java
@@ -14,6 +14,21 @@ public class Pawn extends Piece {
 		HashSet<Integer> moves = new HashSet<Integer>();
 		char[] alloc=aBoard.getAllocation();
 		
+		int cursor=where;
+		if(ChessPiece.Color.BLACK==color){
+			if(8<=where && where<=15)
+				if(alloc[cursor+16]==',')	moves.add(cursor+16);
+			if(alloc[cursor+8]==',')		moves.add(cursor+8);
+			if(Character.isUpperCase(alloc[cursor+7]))	moves.add(cursor+7);
+			if(Character.isUpperCase(alloc[cursor+9]))	moves.add(cursor+9);
+		}else{
+			if(48<=where && where<=55)
+				if(alloc[cursor-16]==',')	moves.add(cursor-16);
+			if(alloc[cursor-8]==',')		moves.add(cursor-8);
+			if(Character.isLowerCase(alloc[cursor-9]))	moves.add(cursor-9);
+			if(Character.isLowerCase(alloc[cursor-7]))	moves.add(cursor-7);
+		}
+		
 		Integer[] crdSet = new Integer[moves.size()];
 		moves.toArray(crdSet);
 		moves = null;
--- src/com/cyrusian/academic/sra/element/Rook.java
+++ src/com/cyrusian/academic/sra/element/Rook.java
@@ -12,6 +12,51 @@ public class Rook extends Piece {
 	@Override
 	public Integer[] getAllAvailableMoves(ChessBoard aBoard, int where) {
 		HashSet<Integer> moves = new HashSet<Integer>();
+		char[] alloc=aBoard.getAllocation();
+		
+		int cursor=where;
+		do {
+			cursor-=8;
+			if(ChessPiece.Handyman.isInBound(cursor)==true) {
+				if(color==ChessPiece.Color.BLACK) {
+					if(alloc[cursor]=='.' || Character.isUpperCase(alloc[cursor])==true)
+						moves.add(cursor);
+					else
+						break;
+				} else {
+					if(alloc[cursor]=='.' || Character.isLowerCase(alloc[cursor])==true)
+						moves.add(cursor);
+					else
+						break;
+				}
+				
+				if(cursor%8==0 || cursor<=7)
+					break;
+			} else
+				break;
+		} while(true);
+		
+		cursor=where;
+		do {
+			cursor+=8;
+			if(ChessPiece.Handyman.isInBound(cursor)==true) {
+				if(color==ChessPiece.Color.BLACK) {
+					if(alloc[cursor]=='.' || Character.isUpperCase(alloc[cursor])==true)
+						moves.add(cursor);
+					else
+						break;
+				} else {
+					if(alloc[cursor]=='.' || Character.isLowerCase(alloc[cursor])==true)
+						moves.add(cursor);
+					else
+						break;
+				}
+				
+				if(cursor%7==0 || cursor>=56)
+					break;
+			} else
+				break;
+		} while(true);
 		
 		Integer[] crdSet = new Integer[moves.size()];
 		moves.toArray(crdSet);
